5a80d78574c7e0a94790f6554d5af5be297d1025
Marius Larsen <mla132@uit.no>
2022-02-06 14:10:17 +0100

    Implemented a simple receiver ref:PS-1

---
+++ pokemon-sightings/src/main/java/org/acme/messaging/PokemonSightingReceiver.java
@@ -0,0 +1,19 @@
+package org.acme.messaging;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.concurrent.CompletionStage;
+
+@ApplicationScoped
+public class PokemonSightingReceiver {
+    private static final Logger LOG = Logger.getLogger(PokemonSightingReceiver.class);
+
+    @Incoming("data-in")
+    public CompletionStage<Void> logPokemonSightings(Message<String> message) {
+        LOG.infov("Pokemon sighting: {0}", message.getPayload());
+        return message.ack();
+    }
+}
